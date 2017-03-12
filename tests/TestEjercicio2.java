f521b76dc7613a4d6a936f258e5fa52acfa0b128
Gonzalo de Achaval <gonzalo.deachaval@ing.austral.edu.ar>
2017-03-12 18:11:12 -0300

    Cambios en el testeo del ejercicio 2

--- tests/TestEjercicio2.java
+++ tests/TestEjercicio2.java
@@ -7,40 +7,35 @@ public class TestEjercicio2 {
     public void evaluatesSelection(){
         Ejercicio2<Integer> ejercicio2 = new Ejercicio2<>();
         Integer[] ints = new Integer[]{9,4,1,5,3,4,10};
-        Integer[] result = ejercicio2.selection(ints);
-        Integer[] answer = new Integer[]{1,3,4,4,5,9,10};
-
-        assertEquals(true,answer==result);
+        printList(ejercicio2.selection(ints));
     }
 
     @Test
     public void evaluatesInsertion2(){
         Ejercicio2<Integer> ejercicio2 = new Ejercicio2<>();
         Integer[] ints = new Integer[]{9,4,1,5,3,4,10};
-        Integer[] result = ejercicio2.insertion(ints);
-        Integer[] answer = new Integer[]{1,3,4,4,5,9,10};
-
-        assertEquals(true,answer==result);
+        printList(ejercicio2.insertion(ints));
     }
 
     @Test
     public void evaluatesBubbleSort(){
         Ejercicio2<Integer> ejercicio2 = new Ejercicio2<>();
         Integer[] ints = new Integer[]{9,4,1,5,3,4,10};
-        Integer[] result = ejercicio2.bubbleSort(ints);
-        Integer[] answer = new Integer[]{1,3,4,4,5,9,10};
-
-        assertEquals(true,answer==result);
+        printList(ejercicio2.bubbleSort(ints));
     }
 
     @Test
     public void recursiveSelection(){
         Ejercicio2<Integer> ejercicio2 = new Ejercicio2<>();
         Integer[] ints = new Integer[]{9,4,1,5,3,4,10};
-        Integer[] result = ejercicio2.recursiveSelection(ints);
-        Integer[] answer = new Integer[]{1,3,4,4,5,9,10};
+        printList(ejercicio2.recursiveSelection(ints));
+    }
 
-        assertEquals(true,answer==result);
+    private void printList(Integer[] list){
+        for(int i=0; i<list.length; i++){
+            System.out.print(list[i] + "\t");
+        }
+        System.out.println();
     }
 
 }
